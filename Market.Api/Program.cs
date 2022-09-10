c409e2b40e5a3dc1ce9730483bb5062ab52576c4
Ozodbek00 <kamoliddinovozodbek@mail.ru>
2022-09-10 11:37:41 +0500

    isProduction

--- Market.Api/Program.cs
+++ Market.Api/Program.cs
@@ -42,7 +42,7 @@ builder.Services.AddScoped<IPurchaseService, PurchaseService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
